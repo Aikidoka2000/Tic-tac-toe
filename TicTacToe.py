8430b2ef592a26b9d68ecccf3d23ce69cf74f61d
Aikidoka2000 <48751242+Aikidoka2000@users.noreply.github.com>
2022-04-12 02:49:22 +0500

    comments

--- TicTacToe.py
+++ TicTacToe.py
@@ -14,37 +14,41 @@ def mapxo():
             print(xomap[i][j], end="")
         print()
 
-def xodo(x, y):
+def xodo(x, y, lane):
     if x == 1 and y == 1:
-        xomap[2][1] = first
+        xomap[2][1] = lane
         return mapxo()
     elif x == 1 and y == 2:
-        xomap[1][1] = first
+        xomap[1][1] = lane
         return mapxo()
     elif x == 1 and y == 3:
-        xomap[0][1] = first
+        xomap[0][1] = lane
         return mapxo()
     elif x == 2 and y == 1:
-        xomap[2][2] = first
+        xomap[2][2] = lane
         return mapxo()
     elif x == 2 and y == 2:
-        xomap[1][2] = first
+        xomap[1][2] = lane
         return mapxo()
     elif x == 2 and y == 3:
-        xomap[0][2] = first
+        xomap[0][2] = lane
         return mapxo()
     elif x == 3 and y == 1:
-        xomap[2][3] = first
+        xomap[2][3] = lane
         return mapxo()
     elif x == 3 and y == 2:
-        xomap[1][3] = first
+        xomap[1][3] = lane
         return mapxo()
     elif x == 3 and y == 3:
-        xomap[0][3] = first
+        xomap[0][3] = lane
         return mapxo()
+
+
 
 
 mapxo()
 for i in range(9):
-    xodo(int(input("x:")), int(input("y:")))
+    xodo(int(input("x:")), int(input("y:")), first)
+    print()
+    xodo(int(input("x:")), int(input("y:")), second)
     print()
